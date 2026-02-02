aede53541bd0e9ab2ee29cd1ecc93269a0032f1f
Tristan Barkman <barkmantristan@gmail.com>
2026-02-02 10:20:11 -0500

    Update collision_model.py

--- collision_model.py
+++ collision_model.py
@@ -1,6 +1,9 @@
 import numpy as np
 
+rng = np.random.default_rng(seed)
+
 def generate_collision_steps(
+    rng: np.random.Generator,
     N: int,
     step_size: float,
     p_forward: float,
@@ -8,13 +11,18 @@ def generate_collision_steps(
     """
     Generate discrete collision steps for random walk
     """
-    return np.random.choice(
+    return np.rng.choice(
         [-step_size, step_size],
         size=N
         p=[1 - p_forward, p_forward],
 )
 
-def generate_collision_velocity(N=10000, step_size=1.0, p_forward=0.5, seed=None)-> np.ndarray:
+def generate_collision_velocity(
+    N: int =10000, 
+    step_size: float =1.0, 
+    p_forward: float =0.5, 
+    seed: int | None =None
+)-> np.ndarray:
     """
     Generates velocity fluctuations using a binomial collision-based random walk.
     
@@ -25,11 +33,10 @@ def generate_collision_velocity(N=10000, step_size=1.0, p_forward=0.5, seed=None
     if not (0.0 <= p_forward <= 1.0):
         raise ValueError("p_forward must be between 0 and 1")
     # This implements a biased binomial random walk in velocity space
-    if seed is not None:
-        np.random.seed(seed)
-    steps = generate_collision_steps(N, step_size, p_forward)
+    steps = generate_collision_steps(rng, N, step_size, p_forward)
     velocity = np.cumsum(steps)
     return velocity
+
 
 
 
