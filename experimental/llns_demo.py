6d589b7b52e5b87cd460d37bc63ddc883873991f
Tristan Barkman <barkmantristan@gmail.com>
2026-04-27 11:39:45 -0400

    Update llns_demo.py

--- experimental/llns_demo.py
+++ experimental/llns_demo.py
@@ -1,6 +1,6 @@
 def collision_step_variance(step_size: float, p_forward: float) -> float:
   """
   Calculates the variance of a single collision step.
-  Based on Bernoulli trial variance.
+  Based on Bernoulli trial variance. 4*p*(1-p)*s^2
   """
   return 4.0 * p_forward * (1.0 - p_forward) * step_size**2
