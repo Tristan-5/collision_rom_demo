c2904e263adc080fd45030cb74a9b6e837d4f05d
Tristan Barkman <barkmantristan@gmail.com>
2026-04-27 09:36:24 -0400

    Add collision_step_variance function
    Adds a function to calculate variance of a collision step based on Bernoulli trials.

--- experimental/llns_demo.py
+++ experimental/llns_demo.py
@@ -1,1 +1,6 @@
-
+def collision_step_variance(step_size: float, p_forward: float) -> float:
+  """
+  Calculates the variance of a single collision step.
+  Based on Bernoulli trial variance.
+  """
+  return 4.0 * p_forward * (1.0 - p_forward) * step_size**2
